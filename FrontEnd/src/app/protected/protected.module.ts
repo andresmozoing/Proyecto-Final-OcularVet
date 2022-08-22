a84e05bedd9a762b7a857a195b74096b9ca46d22
Facundo Mozo <fmozo@alumnos.exa.unicen.edu.ar>
2022-08-22 12:50:34 -0300

    cambios protected module

--- FrontEnd/src/app/protected/protected.module.ts
+++ FrontEnd/src/app/protected/protected.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule ,  ReactiveFormsModule} from '@angular/forms';
-import { CountdownModule } from 'ngx-countdown';
+
 
 import { ProtectedRoutingModule } from './protected-routing.module';
 import { MatTableModule } from '@angular/material/table';
