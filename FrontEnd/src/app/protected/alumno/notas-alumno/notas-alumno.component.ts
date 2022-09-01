b62ab4f440cfcc2f742088acd6b178efc8d9abbc
Facundo Mozo <fmozo@alumnos.exa.unicen.edu.ar>
2022-09-01 13:40:55 -0300

    Cambio de aspectos de todas las tablas que se visualizan en la app Cambio de la logica utilizada para el Sort de todas las tablas Cambio de aspecto del titulo de pagina perfil para que coincida con demas secciones de la app  Se agrego la funcionalidad de eliminar una nota del lado del admin en lka pantalla de notas  Agregado el metodo en el service para la eliminacion de notas  Correccion de notaController para utilizar datos pasados en el header  y no datos del body en metodo eliminar nota

--- FrontEnd/src/app/protected/alumno/notas-alumno/notas-alumno.component.ts
+++ FrontEnd/src/app/protected/alumno/notas-alumno/notas-alumno.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { NotaService } from '../../services/nota.service';
 import { AuthService } from '../../../auth/services/auth.service';
 import { Nota } from '../../interfaces/Nota';
-import { MatTableDataSource } from '@angular/material/table';
-import {MatSort, Sort} from '@angular/material/sort';
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-// import { ClaseNota } from './notas-alumno.component';
 
 
 
@@ -15,40 +11,108 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
 })
 export class NotasAlumnoComponent implements AfterViewInit {
   notas!: Nota[];
-  
-  displayedColumns: string[] = ['LU', 'fecha', 'rtasCorrectas','cantidadPreguntas','calificacion']; 
-  datos: Nota[] = [  ];
-  dataSource = new MatTableDataSource(this.datos);
+  ordenActual: string = "LUAsc";
   
 
   constructor( private authservice: AuthService,
-    private notaService : NotaService,
-    private _liveAnnouncer: LiveAnnouncer) {}
+    private notaService : NotaService) {}
     
-    @ViewChild(MatSort) sort!: MatSort;
     
     ngAfterViewInit() {
-      this.notasAlumno();
-      this.dataSource.sort = this.sort;      
+      this.notasAlumno(); 
     }
   notasAlumno(){
     return this.notaService.obtenerNotasUsuario(
                   this.authservice.usuario.uid,
                   this.authservice.usuario.LU
     ).subscribe( (resp)=>{
-
-      this.dataSource = new MatTableDataSource(resp.notas);
-
-      this.dataSource.sort = this.sort;
+      this.notas = resp.notas;
+      
     });
   }
   
   
-  announceSortChange(sortState: Sort) {  
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
+  sortTabla(ordenNuevo: string, recarga : boolean = false) {
+    if (recarga){ 
+      //Cuando eliminamos una nota queremos que mantenga el orden
+      this.ordenActual="";
     }
+    if (ordenNuevo === this.ordenActual) {  //Significa que selecciono dos veces el mismo, cambiamos de Dsc a Asc
+      if(this.ordenActual.includes("Asc")){
+        ordenNuevo = ordenNuevo.replace("Asc","Dsc") 
+      }
+    }  
+    this.ordenActual = ordenNuevo //Actualizamos el ordenActual
+    switch (ordenNuevo) { 
+      //Descendentes
+      case 'LUDsc':
+        this.notas.sort((a, b) => b.LU! - a.LU!);
+        break
+      case 'nameDsc':
+        this.notas.sort((a, b) => {
+          var nameA = a.name!.toLowerCase(), nameB = b.name!.toLowerCase();
+          if (nameA < nameB)
+            return 1;
+          if (nameA > nameB)
+            return -1;
+          return 0;
+        });
+        break
+      case 'surnameDsc':
+        this.notas.sort((a, b) => {
+          var surnameA = a.surname!.toLowerCase(), surnameB = b.surname!.toLowerCase();
+          if (surnameA < surnameB)
+            return 1;
+          if (surnameA > surnameB)
+            return -1;
+          return 0;
+        });
+        break
+      case 'rtasCorrectasDsc':
+        this.notas.sort((a, b) => b.rtasCorrectas! - a.rtasCorrectas!);
+        break
+      case 'cantidadPreguntasDsc':
+        this.notas.sort((a, b) => b.cantidadPreguntas! - a.cantidadPreguntas!);
+        break
+      case 'calificacionDsc':
+        this.notas.sort((a, b) => b.calificacion! - a.calificacion!);
+        
+        break
+      //Ascendentes
+      case 'LUAsc':
+        this.notas.sort((a, b) => a.LU! - b.LU!);
+        break
+      case 'nameAsc':
+        this.notas.sort((a, b) => {
+          var nameA = a.name!.toLowerCase(), nameB = b.name!.toLowerCase();
+          if (nameA < nameB)
+            return -1;
+          if (nameA > nameB)
+            return 1;
+          return 0;
+        });
+        break
+      case 'surnameAsc':
+        this.notas.sort((a, b) => {
+          var surnameA = a.surname!.toLowerCase(), surnameB = b.surname!.toLowerCase();
+          if (surnameA < surnameB)
+            return -1;
+          if (surnameA > surnameB)
+            return 1;
+          return 0;
+        });
+        break
+      case 'rtasCorrectasAsc':
+        this.notas.sort((a, b) => a.rtasCorrectas! - b.rtasCorrectas!);
+        break
+      case 'cantidadPreguntasAsc':
+        this.notas.sort((a, b) => a.cantidadPreguntas! - b.cantidadPreguntas!);
+        break
+      case 'calificacionAsc':
+        this.notas.sort((a, b) => a.calificacion! - b.calificacion!);
+        break
+        
+    }
+
   }
 }
